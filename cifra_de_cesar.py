4341d44fa83ac99441b869cdb5c58ded784b9381
Barbara Fernandes <https://github.com/BarbaraAFernandes>
2020-04-21 00:59:54 -0300

    terminei o código

--- cifra_de_cesar.py
+++ cifra_de_cesar.py
@@ -1,7 +1,9 @@
+import requests
+
 alfabeto = 'abcdefghijklmnopqrstuvwxyz'
-
-
-
+url_requisito = 'https://api.codenation.dev/v1/challenge/dev-ps/generate-data?token=0ae42f373d8243a613858a0d58bef7ecd3799228'
+url_resposta = 'https://api.codenation.dev/v1/challenge/dev-ps/submit-solution?token=0ae42f373d8243a613858a0d58bef7ecd3799228'
+answer = 'C:\\Users\\Barbara\\Desktop\\codenation\\codenation\\answer.json'
 
 def encriptar(rotacao, mensagem):
     resultado = ''
@@ -25,7 +27,7 @@ def decriptar(rotacao, mensagem):
     print(resultado)
 
 def decida():
-    escolha = int(input(" 1 Encriptar: \n 2 Decriptar: "))
+    escolha = int(input(" 1 Encriptar: \n 2 Decriptar: \n 3 Requisição: \n 4 Enviar: "))
     if escolha == 1:
         rotacao = int(input("Decida a rotação: "))
         mensagem = input('Escreva uma mensagem: ')
@@ -34,8 +36,20 @@ def decida():
         rotacao = int(input("Decida a rotação: "))
         mensagem = input('Escreva uma mensagem: ')
         decriptar(rotacao, mensagem)
+    elif escolha == 3:
+        requisito(url_requisito)
+    elif escolha == 4:
+        enviar(url_resposta)
+
+def requisito(url_requisito):
+    resposta = requests.get(url_requisito)
+    with open(answer, 'w+') as arquivo:
+        arquivo.write(resposta.text) 
+        print('status: ', resposta.status_code)
+
+def enviar(url_resposta):
+    arquivo = {'answer': ('answer.json', open(answer, 'rb'))}
+    resposta = requests.post(url_resposta, files=arquivo)
+    print('status: ', resposta.status_code)
 
 decida()
-
-
-    
